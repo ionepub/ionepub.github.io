18a96a02c14949e4b08de2423a884be882b1765e
ionepub <lanzl@netpai.com>
2016-09-04 16:28:51 +0800

    添加主程序

---
+++ js/config.js
@@ -0,0 +1,9 @@
+var CONFIG = {
+    owner        : 'ionepub',
+    repo         : 'ionepub.github.io',
+    duoshuo      : 'ioneblog',
+    baiduAnaly   : '33ad3310cedb8c9c1788da527686f49f',
+    access_token : 'eca89e0fd564088457a2957f350b12ecdc4b8bc7'
+};
+
+var duoshuoQuery = {short_name: CONFIG['duoshuo']};
