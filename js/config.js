201797c9d094665a0ad829fed87ba7dc131d461c
ionepub <lanzl@netpai.com>
2016-09-04 17:06:59 +0800

    去掉access token

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ var CONFIG = {
     repo         : 'ionepub.github.io',
     duoshuo      : 'ioneblog',
     baiduAnaly   : '6917e4c5a39811dc77df111fcc5e1674',
-    access_token : 'eca89e0fd564088457a2957f350b12ecdc4b8bc7'
+    //access_token : 'eca89e0fd564088457a2957f350b12ecdc4b8bc7'
 };
 
 var duoshuoQuery = {short_name: CONFIG['duoshuo']};
