d4101e01dc6b2fa64e7e4e5386d9ac6449a8d740
Jerry Lan <1577628623@qq.com>
2017-05-05 18:34:39 +0800

    关闭多说评论

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 var CONFIG = {
     owner        : 'ionepub',
     repo         : 'ionepub.github.io',
-    duoshuo      : 'ioneblog',
+    //duoshuo      : 'ioneblog',
+    duoshuo      : '',
     baiduAnaly   : '6917e4c5a39811dc77df111fcc5e1674',
     //access_token : 'eca89e0fd564088457a2957f350b12ecdc4b8bc7'
 };
